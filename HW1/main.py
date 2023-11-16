bd22cab5f7bb32665df6d7cf11e912de9f650cbc
Sivvak <pawel.7p@gmail.com>
2023-11-16 22:20:24 +0100

    Added bayesian optimization, moved old results to archive folder, minor changes

--- HW1/main.py
+++ HW1/main.py
@@ -1,19 +1,28 @@
 ## load libraries
 import openml
 import os
+import sys
 import numpy as np
 import pandas as pd
+from bayes_initial_params import get_bayes_initial_params
+from scipy.stats import uniform, randint
 from sklearn import set_config
 from sklearn.compose import make_column_selector, ColumnTransformer
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import mean_squared_error, accuracy_score, roc_auc_score
-from sklearn.model_selection import train_test_split, GridSearchCV, KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from skopt import BayesSearchCV
 
 set_config(transform_output = "pandas")
+
+hw1_path = os.path.join(os.path.expanduser("~"), f'Desktop\\AutoML\\HW1')
+
+run_random = '--random' in sys.argv
+run_bayes = '--bayes' in sys.argv
+
 
 def run_train_iteration(dataset_id: int, target_column_name: str, model, params: dict):
     ## load dataset
@@ -24,9 +33,6 @@ def run_train_iteration(dataset_id: int, target_column_name: str, model, params:
     X, _, _, _ = dataset.get_data(dataset_format="dataframe")
     y = X.loc[:, target_column_name]
     X = X.drop([target_column_name], axis = 1)
-
-    ## split data into train and test
-    # X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     ## define pipeline
     col_trans = ColumnTransformer(transformers=[
@@ -40,17 +46,30 @@ def run_train_iteration(dataset_id: int, target_column_name: str, model, params:
     model_pipeline = Pipeline([('preprocessing', col_trans),
                             ('model', model)])
 
-    ## optimize model using grid search
-    grid_search = GridSearchCV(model_pipeline, params, cv=5, scoring='accuracy', n_jobs=-1, verbose=2)
-    grid_search.fit(X, y)
+    if run_random:
+        ## tune hyperparams using grid search
+        grid_search = GridSearchCV(model_pipeline, params, cv=5, scoring='accuracy', n_jobs=-1, verbose=2)
+        grid_search.fit(X, y)
 
-    ## save results to csv
-    path = os.path.join(os.path.expanduser("~\\Desktop"), f'AutoML\\HW1\\results\\{type(model).__name__}\\{dataset.name}.csv')
+        ## save results to csv
+        path = os.path.join(hw1_path, f'results\\{type(model).__name__}\\random\\{dataset.name}.csv')
 
-    results = pd.DataFrame(grid_search.cv_results_)
-    results.to_csv(path)
+        results = pd.DataFrame(grid_search.cv_results_)
+        results.to_csv(path)
+
+    if run_bayes:
+        ## tune hyperparams using bayesian optimization
+        bayes_search = BayesSearchCV(model_pipeline, params, cv=5, scoring='accuracy', n_jobs=-1, verbose=2)
+        bayes_search.fit(X, y)
+
+        ## save results to csv
+        path = os.path.join(hw1_path, f'results\\{type(model).__name__}\\bayes\\{dataset.name}.csv')
+
+        results = pd.DataFrame(bayes_search.cv_results_)
+        results.to_csv(path)
 
     print(f'Training completed for {type(model).__name__} on dataset {dataset.name}')
+
 
 if __name__ == '__main__':
     datasets = [
@@ -60,14 +79,18 @@ if __name__ == '__main__':
         (334, 'class')
     ]
 
+    sample_size = 3
+    svc_C = [2 ** x for x in uniform(loc=-10, scale=20).rvs(sample_size)]
+    svc_gamma = [2 ** x for x in uniform(loc=-10, scale=20).rvs(sample_size)]
+
     algorithms = [
         (
             DecisionTreeClassifier(),
             {
-                'model__ccp_alpha': [0, 0.2, 0.4, 0.6, 0.8, 1],
-                'model__max_depth': [1, 5, 10, 15, 20, 25, 30],
-                'model__min_samples_leaf': [1, 10, 20, 30, 40, 50, 60],
-                'model__min_samples_split': [2, 10, 20, 30, 40, 50, 60],
+                'model__ccp_alpha': uniform().rvs(sample_size),
+                'model__max_depth': randint(1, 31).rvs(sample_size),
+                'model__min_samples_leaf': randint(1, 61).rvs(sample_size),
+                'model__min_samples_split': randint(1, 61).rvs(sample_size)
             }
         ),
         (
@@ -75,37 +98,38 @@ if __name__ == '__main__':
             [
                 {
                     'model__kernel': ['linear'],
-                    'model__C': [0.1, 1, 10, 100, 1000]
+                    'model__C': svc_C
                 },
                 {
                     'model__kernel': ['rbf'],
-                    'model__C': [0.1, 1, 10, 100, 1000],
-                    'model__gamma': [0.001, 0.0001]
+                    'model__C': svc_C,
+                    'model__gamma': svc_gamma
                 },
                 {
                     'model__kernel': ['sigmoid'],
-                    'model__C': [0.1, 1, 10, 100, 1000],
-                    'model__gamma': [0.001, 0.0001]
+                    'model__C': svc_C,
+                    'model__gamma': svc_gamma
                 },
                 {
                     'model__kernel': ['poly'],
-                    'model__C': [0.1, 1, 10, 100, 1000],
-                    'model__gamma': [0.001, 0.0001],
-                    'model__degree': [2, 3, 4, 5]
+                    'model__C': svc_C,
+                    'model__gamma': svc_gamma,
+                    'model__degree': randint(2, 6).rvs(sample_size)
                 }
             ]
         ),
         (
             GradientBoostingClassifier(),
             {
-                'model__learning_rate': [0, 0.2, 0.4, 0.6, 0.8, 1],
-                'model__n_estimators': [1, 100, 1000, 2000, 3000, 4000, 5000],
-                'model__subsample': [0.1, 0.3, 0.5, 0.7, 0.9, 1],
-                'model__max_depth': [1, 5, 10, 15, 20, 25, 30],
+                'model__learning_rate': [2 ** x for x in uniform(loc=-10, scale=10).rvs(sample_size)],
+                'model__n_estimators': randint(1, 5001).rvs(sample_size),
+                'model__subsample': uniform(loc=0.1, scale=0.9).rvs(sample_size),
+                'model__max_depth': randint(1, 31).rvs(sample_size),
             }
         )
     ]
 
+    ## tune hyperparameters
     for dataset_id, target_column_name in datasets:
         for model, params in algorithms:
             run_train_iteration(dataset_id, target_column_name, model, params)
